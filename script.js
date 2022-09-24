99e54334c88581019fdf22faf2a2f3636460c6fc
Mr.Demir <55161523+Mr-DEM1R@users.noreply.github.com>
2022-09-24 12:53:07 +0100

    Update script.js

--- script.js
+++ script.js
@@ -63,26 +63,35 @@ test("There is at least one image", () => {
 test("There are at least 500 words on the page", () => {
   expect(getWords.length).toBeGreaterThanOrEqual(500);
 });
-var fruits, emoji;
+var total_others, total_sheep;
 
-
-fruits = ['banana', 'Coconut', 'strawberry'];
-emoji = ['🍌', '🥥', '🍓'];
-let element_list = document.getElementById('list');
-while (!!fruits.length) {
-  if(--window.LoopTrap <= 0) throw "Infinite loop.";
-  let new_li = document.createElement('li');
-  let new_span = document.createElement('span');
-  new_span.innerText = fruits.shift();
-
-  new_li.appendChild(new_span);
-
-  element_list.appendChild(new_li);
-  let new_span2 = document.createElement('span');
-  new_span2.innerText = emoji.shift();
-
-  element_list.appendChild(new_span2);
+function getNumberOrString(value) {
+  // Convert a string value to a number if possible
+  let number_value = Number(value);
+  if (Number.isNaN(number_value)) {
+    return value
+  } else {
+    return number_value
+  }
 }
 
+
+total_others = 0;
+
+total_sheep = 0;
+
+
+document.getElementById('button').addEventListener('click', (event) => {
+  let element_sheep_count = document.getElementById('sheep_count');
+  element_sheep_count.innerText = total_sheep;
+  if (getNumberOrString(document.getElementById('text').value) == 'sheep') {
+    total_sheep = (typeof total_sheep === 'number' ? total_sheep : 0) + 1;
+  } else {
+    let element_other_count = document.getElementById('other_count');
+    element_other_count.innerText = total_others;
+    total_others = (typeof total_others === 'number' ? total_others : 0) + 1;
+  }
+
+});
 const console = document.getElementById("tests");
 prettify.toHTML(run(), console);
