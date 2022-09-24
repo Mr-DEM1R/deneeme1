00bfb092d680aee7df5082f556decaa05f866113
Mr.Demir <55161523+Mr-DEM1R@users.noreply.github.com>
2022-09-24 14:54:25 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,16 @@
-var total_others, total_sheep;
-
-function getNumberOrString(value) {
-  // Convert a string value to a number if possible
-  let number_value = Number(value);
-  if (Number.isNaN(number_value)) {
-    return value
-  } else {
-    return number_value
-  }
-}
+var fruits;
 
 
-total_others = 0;
-
-total_sheep = 0;
+fruits = ['orange', 'mango', 'banana'];
 
 
 document.getElementById('button').addEventListener('click', (event) => {
-  let element_sheep_count = document.getElementById('sheep_count');
-  element_sheep_count.innerText = total_sheep;
-  if (getNumberOrString(document.getElementById('text').value) == 'sheep') {
-    total_sheep = (typeof total_sheep === 'number' ? total_sheep : 0) + 1;
-  } else {
-    let element_other_count = document.getElementById('other_count');
-    element_other_count.innerText = total_others;
-    total_others = (typeof total_others === 'number' ? total_others : 0) + 1;
+  if (!!fruits.length) {
+    let element_list = document.getElementById('list');
+    let new_li = document.createElement('li');
+    new_li.innerText = fruits.shift();
+
+    element_list.appendChild(new_li);
   }
 
 });
-
-});
-const console = document.getElementById("tests");
-prettify.toHTML(run(), console);
